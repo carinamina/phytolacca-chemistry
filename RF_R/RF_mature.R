86cad94df8bf8d7b01289cad15f5073172441050
Carina Baskett <baskettc@msu.edu>
2021-03-22 14:07:11 +0100

    oops, realized the scores are important so i put them back. also worked a bit on further analysis of the mature leaves following the model selection steps.

--- RF_R/RF_mature.R
+++ RF_R/RF_mature.R
@@ -66,7 +66,7 @@ system("python3.9 ./RF_python_scripts/ML_regression.py -df ./RF_R/RF_mature_tab.
 #system("python3.9 ./RF_python_scripts/ML_regression.py -df ./RF_R/RF_mature_tab.csv -alg RF -y_name area -gs T -cv 2 -n 3 -save ./RF_R/testsave -feat ./RF_R/test_features.txt")
 
 #########################
-# visualizing results and removing features for next round
+# visualizing results and removing features for next iteration; running next iteration
 #########################
 # For each step, need to change 7 things: the inputs for feat_plot (2) and new_list (3) and system (2). The larger, previous number goes in the first three slots, and the smaller new number goes in the last 4.
 
@@ -198,9 +198,34 @@ plot(`R^2` ~ features, data = feat_plot, xlab = "Number of features", ylab = exp
 dev.off()
 
 #########################
-# run a "final final" model that uses the feature list from the best model but runs it on a larger dataset
+# run a "final final" model that uses the feature list from the best model but runs it on a larger dataset. also compare models with and without pop.
 #########################
 
+#use feature list from model with 21 features. Might go back and explore the space between 63 and 16 better, but for now this is the best model.
+
+feat_list <- read.csv("RF_R/mature_21_features.txt", header=F)$V1
+
+bigger <- read.csv("Processing/2_out_AllTraits.csv",header=T) %>% filter(age == "mature") %>% select(c(pop,line,area,all_of(feat_list)))
+
+#change pop to dummy variable. not sure why I had trouble doing this in the same line as initially creating mature
+bigger <- bind_cols(bind_cols(bigger[2:3],as.data.frame(to.dummy(bigger$pop, "pop"))),bigger[4:ncol(bigger)]) %>% select(-pop)
+#check that all lines are unique (this is the unique ID now)
+length(unique(bigger$line)) == nrow(bigger)
+#check where the NAs are and how many samples are lost
+sum(is.na(bigger$"X3.91_1011.4733m.z"))
+nrow(bigger) - nrow(na.omit(bigger))
+#6 area, 3 for LC/MS stuff. 9 rows are lost when NAs are removed...so we gain one datapoint compared to the 74 lines used for all traits. This is definitely not worth the hassle of explaining that we used a "bigger" dataset for the "final" model, but let's see what happens for young before going back and getting rid of this step. The pop thing needs to happen anyway.
+
+
+
+
+
+
+
+
+
+#write the dataset as tab-delimited for python to use, removing NAs
+#write.table(na.omit(bigger), "RF_R/RF_bigger_tab.csv",row.names=F,sep="\t")
 
 # 
 # 
@@ -208,7 +233,7 @@ dev.off()
 # # setEPS()
 # # postscript("20180509_mature_chem_scores.eps")
 # # par(mar=c(5,5,4,2))
-# # plot(Y ~ Mean, data = read.csv("mature_20180507_RF_chem13_scores.txt", sep = "\t", header=TRUE), xlab = "Predicted values", ylab = "Actual values", main = "C) Fit of best model", cex.lab=1.75, cex.axis=1.75, cex.main=1.75, cex.sub=1.75)
+plot(Y ~ Mean, data = read.csv("RF_R/mature_21_scores.txt", sep = "\t", header=TRUE), xlab = "Predicted values", ylab = "Actual values", main = "C) Fit of best model", cex.lab=1.75, cex.axis=1.75, cex.main=1.75, cex.sub=1.75)
 # # dev.off()
 # 
 # rm(feat_plot)
