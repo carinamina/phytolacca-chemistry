01fa62b48f86fe8b431cf8b8d333066cc4aa63ff
Carina Baskett <baskettc@msu.edu>
2021-05-10 09:51:19 +0200

    results for both leaf ages

--- RF_R/combined_results.R
+++ RF_R/combined_results.R
@@ -8,9 +8,9 @@ library(tidyverse)
 #for now I haven't been able to beat the original mature 15 with small steps ("RF_R/mature_15_imp"). the best young is "RF_R/youngfine_45_imp"
 
 #can't figure out how to do these in one step because the ranking refers to the row names of the table and it has to exist already
-young <- read.csv("RF_R/youngfine_45_imp",sep = "\t", header = T) %>% rename(feature=X,imp=mean_imp) 
+young <- read.csv("RF_R/youngfine_24_imp",sep = "\t", header = T) %>% rename(feature=X,imp=mean_imp) 
 young <- young %>% add_column(final_rank = as.numeric(row.names(young)), age= "young")
-mature <- read.csv("RF_R/mature_15_imp",sep = "\t", header = T) %>% rename(feature=X,imp=mean_imp) 
+mature <- read.csv("RF_R/maturefine_9_imp",sep = "\t", header = T) %>% rename(feature=X,imp=mean_imp) 
 mature <- mature %>% add_column(final_rank = as.numeric(row.names(mature)), age= "mature")
 
 #number of unique features
@@ -19,15 +19,15 @@ length(unique(bind_rows(mature,young)$feature))
 #features on both lists
 (overlap = intersect(young$feature,mature$feature))
 
-#wow, only 3 compounds overlap! That is nuts. what is their rank in each age?
+#2 compounds and latitude overlap
 mature[mature$feature %in% overlap==T,]
 young[young$feature %in% overlap==T,]
-#two of the top 3 in mature (not #1) are also found in the top third of important chemicals in young leaves. Another is lower on both lists. Otherwise, the compounds found to be important predictors of young and mature leaves are unique to each leaf age.
+#two of the top 3 in mature (not #1) are also found to be important chemicals in young leaves
 rm(overlap)
 ###############
 #how did the non-chemical features rank in the maximal model? And how did the chemicals in the final model rank in the original model? I was going to ask when did the non-chemical features drop out during model selection, but realized that maybe answering this easier question is sufficient. Especially when you see how little the rankings actually change--the original model is amazingly good at picking up signal through the noise!
 ###############
-nonchem <- c("lat", "tough","percent_N","C_N","log.abund","richness","diversity","reg.north_temperate","reg.subtropical","reg.temperate","reg.tropical")
+nonchem <- c("lat", "tough","percent_N","C_N","log.abund","richness","diversity","reg.north_temperate","reg.subtropical","reg.temperate","reg.tropical","spp.PHRI")
 
 orig <- bind_rows(mature,young) %>% select(-imp) %>% add_row(feature = nonchem[2:length(nonchem)], final_rank = NA, age= "young") %>% add_row(feature = nonchem, final_rank = NA, age= "mature") %>% arrange(age) %>% add_column(orig_rank = NA)
 
